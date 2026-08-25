d5e2022315d39631ec8c79a595ec0a7384436e55
0x1402h0u <2735190799@qq.com>
2026-08-25 08:59:09 +0000

    Delete Ampulos directory

--- Ampulos/app/src/main/java/com/android/ampulos/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.android.ampulos.navigation
-
-sealed class Screen(val route: String) {
-    object Splash : Screen("splash")
-    object Draw : Screen("draw")
-    object Profile : Screen("profile")
-    object DeckConfig : Screen("deck_config")
-}
